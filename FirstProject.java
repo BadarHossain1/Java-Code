f728e121a91409fa796524226b5969b50201d6ad
Badar Hossain <badar12041@gmail.com>
2024-07-12 15:37:13 +0600

    A program to find Hypotenuse

---
+++ FirstProject.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        double x; 
+        double y;
+        double z;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter Side X: ");
+        x = scanner.nextDouble();
+
+        System.out.println("Enter Side Y: ");
+        y = scanner.nextDouble();
+
+        z = Math.sqrt((x*x)+(y*y));
+
+        System.out.println("The Hypotenuse is  = " + z);
+
+
+        scanner.close();
+        
+    }
+}

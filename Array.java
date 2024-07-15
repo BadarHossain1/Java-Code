428cf93a18c1a05e4e2ad0a3fc5a3acd8bfae66c
Badar Hossain <badar12041@gmail.com>
2024-07-15 20:10:43 +0600

    Java Class 2 code

---
+++ Array.java
@@ -0,0 +1,21 @@
+ public class Array {
+    public static void main (String[] args){
+
+        int[] array = {1,2,3,5,6,8,8,9};
+
+        for(int i: array){
+            System.out.println(i);
+        }
+
+
+        String[] string = {"mercedes", "corvette", "BMW"};
+
+        for(String i: string){
+            System.out.println(i);
+        }
+
+
+;
+
+}
+}
---
+++ Main.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+
+    for(int i = 0; i<10; i++){
+      System.out.println("Digit: " + i);
+    }
+
+    int arr[] = {1,2,3,4,5,6,7,8,9,10};
+    
+    
+    for(int i: arr){
+      System.out.println(i);
+    }
+
+
+    int count = 0 ;
+
+    for(int i = 0; i<=100; i++){
+      count ++;
+    }
+
+    System.out.println("Count: " + count);
+
+    
+
+
+    
+
+    
+
+
+    
+  }
+
+
+}

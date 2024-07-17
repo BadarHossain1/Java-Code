2f8250901c37dab6228dc4b3421c52fb28fff913
Badar Hossain <badar12041@gmail.com>
2024-07-17 11:54:25 +0600

    String and String methods

---
+++ Strings.java
@@ -0,0 +1,11 @@
+public class Strings {
+
+    public static void main(String[] args){
+        //String = a reference data type that can store one or more characters reference data types have access to useful methods
+
+        String name = "John";
+
+        
+    }
+    
+}
